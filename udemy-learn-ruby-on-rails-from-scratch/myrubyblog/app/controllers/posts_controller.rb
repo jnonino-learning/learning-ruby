a195fd6b98c1033a8ef881b73dc8b29ef4bf503a
Julian Nonino <noninojulian@gmail.com>
2019-07-24 10:48:24 +0100

    Controllers, Views and Routes
    Creating a posts controller and basic views for each of the actions

---
+++ udemy-learn-ruby-on-rails-from-scratch/myrubyblog/app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @content_first = 'This is some sample text for our awsome new Ruby Blog'
+    @content_second = 'This is more sample text for our awsome new Ruby Blog'
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+end

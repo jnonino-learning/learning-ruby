f0ce0d86a5f0a47757af1f3966c725bb21cafb1d
Julian Nonino <noninojulian@gmail.com>
2019-07-24 17:23:20 +0100

    Working with Models and Database
    Database Planning and Model Creation - Parts 1 & 2
    Database Association

--- udemy-learn-ruby-on-rails-from-scratch/myrubyblog/config/routes.rb
+++ udemy-learn-ruby-on-rails-from-scratch/myrubyblog/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'  
   get 'home/index'
     resources :posts
+    resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

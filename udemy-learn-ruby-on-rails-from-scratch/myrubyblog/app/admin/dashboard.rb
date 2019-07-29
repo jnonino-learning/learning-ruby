d8437e7a05c7403bafa01b8a45c12e2589f0caca
Julian Nonino <noninojulian@gmail.com>
2019-07-29 10:16:31 +0100

    Scaffolding and Active Admin
    Configuring Active Admin and Database import

--- udemy-learn-ruby-on-rails-from-scratch/myrubyblog/app/admin/dashboard.rb
+++ udemy-learn-ruby-on-rails-from-scratch/myrubyblog/app/admin/dashboard.rb
@@ -2,12 +2,12 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
@@ -28,5 +28,31 @@ ActiveAdmin.register_page "Dashboard" do
     #     end
     #   end
     # end
+
+    section "Recent Posts" do
+      table_for Post.order("id desc").limit(15) do
+        column :id
+        column "Post Title", :title do |post|
+          link_to post.title,[:admin,post]
+        end
+        column :category,:sortable => :category
+        column :created_at
+      end
+      strong (link_to "Show all posts",:admin_posts)
+    end
+
+
+
+    section "Categories" do
+      table_for Category.order("id desc").limit(15) do
+        column :id
+        column "Category Name", :name do |category|
+          link_to category.name,[:admin,category]
+        end
+        column :created_at
+      end
+      strong (link_to "Show all categories",:admin_categories)
+    end
+
   end # content
 end

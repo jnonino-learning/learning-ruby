88807d29eabca2e2e1cc76fcd28639287b6644c4
Julian Nonino <noninojulian@gmail.com>
2019-07-29 12:25:57 +0100

    Layout, Presentation and Testing
    Intro to Layouts
    Static Pages and Navigation

--- udemy-learn-ruby-on-rails-from-scratch/myrubyblog/config/routes.rb
+++ udemy-learn-ruby-on-rails-from-scratch/myrubyblog/config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+  get 'pages/contact'
+  get 'pages/resources'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'home#index'  
   get 'home/index'
     resources :posts
     resources :categories
+    match '/about', :to => 'pages#about', via: :all
+    match '/contact', :to => 'pages#contact', via: :all
+    match '/resources', :to => 'pages#resources', via: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
